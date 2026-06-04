ed1daf108d70e8f364c68244cf3219faeb6d3d49
roop <roop25327@gmail.com>
2026-06-04 15:48:00 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base:'/ohgims/'
+  base:'/ohgims/',
   plugins: [
     react(),
     tailwindcss(),
