9df119bcf08011d41b4147f1787545942ccb0412
roop <roop25327@gmail.com>
2026-06-04 16:43:12 +0530

    browserrouting

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import ProtectedRoute from '../components/shared/ProtectedRoute'
 import Login from './login'
@@ -22,7 +22,7 @@ function RootRedirect() {
 
 export default function Router() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<RootRedirect />} />
         <Route path="/login" element={<Login />} />
@@ -66,6 +66,6 @@ export default function Router() {
           </ProtectedRoute>
         } />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
