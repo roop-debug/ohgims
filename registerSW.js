c8548f10ff03549723bed0e02c06b768c5696448
roop-debug <roop-debug@users.noreply.github.com>
2026-06-04 11:18:48 +0000

    deploy: 1423a410e17ccea82c63501ab7c45839c16b1c7a

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ohgims/sw.js', { scope: '/ohgims/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
